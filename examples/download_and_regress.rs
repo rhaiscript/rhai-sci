de89b6f47be1ac3cab12cecdc69d50a13d20031e
Chris McComb <ccmcc2012@gmail.com>
2022-07-20 19:54:07 -0400

    Now with example

---
+++ examples/download_and_regress.rs
@@ -0,0 +1,18 @@
+use rhai::{packages::Package, Dynamic, Engine, INT};
+use rhai_sci::SciPackage;
+
+fn main() {
+    // Create a new Rhai engine
+    let mut engine = Engine::new();
+
+    // Add the rhai-sci package to the new engine
+    engine.register_global_module(SciPackage::new().as_shared_module());
+
+    // Now run your code
+    println!(
+        "{:?}",
+        engine
+            .eval_file::<Dynamic>("examples/download_and_regress.rhai".into())
+            .unwrap()
+    );
+}
